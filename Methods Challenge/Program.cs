431e10ef4795461674a640a5bd4abdc6dfd9ac2b
davidmrtn07 <davidmrtn07@gmail.com>
2024-03-20 09:33:24 -0500

    Add project files.

---
+++ Methods Challenge/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+    namespace MethodsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+        //Write a method that will print to the console numbers 1000 through -1000
+        public static void PrintThousand()
+        {
+            for(int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
